b7a0c77dbda11c2b3bbd5954e0e200b4a81f19f8
Giovanni Pace <g.pace@braintechinnovations.it>
2026-01-29 11:53:22 +0100

    TEST

--- lez02_anagrafe.sql
+++ lez02_anagrafe.sql
@@ -10,4 +10,21 @@ CREATE TABLE Persona(
     telefono VARCHAR(50)
 );
 
-ALTER TABLE Persona ADD COLUMN  email VARCHAR(250) NOT NULL;
+ALTER TABLE Persona ADD COLUMN email VARCHAR(250) NOT NULL;
+ALTER TABLE Persona ADD COLUMN altezza FLOAT AFTER cognome;
+ALTER TABLE Persona DROP COLUMN altezza;
+ALTER TABLE Persona MODIFY COLUMN email VARCHAR(100);
+
+-- DML
+INSERT INTO Persona (nome, cognome, cod_fis, telefono)
+VALUES
+('Luca', 'Rossi', 'RSL123456B', '3456789012'),
+('Giulia', 'Verdi', 'VRD987654C', '0987654321'),
+('Alessandro', 'Bianchi', 'BLH543210D', '8765432109'),
+('Francesca', 'Migliori', 'MGZ321099E', '1234567890'),
+('Matteo', 'Gialli', 'GLL890123F', '0987654321'),
+('Sara', 'Bassi', 'BSN654321G', '9876543210'),
+('Marco', 'Rosso', 'RSM321094H', '7654321098'),
+('Lorenzo', 'Grigi', 'GRD549834I', '1234567890'),
+('Elena', 'Blu', 'BLU278949J', '0987654321'),
+('Giorgio', 'Neri', 'NER976543K', '9876543210');
