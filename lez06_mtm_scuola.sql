7841a5ced1dc27611104d0832db2d51be3636924
Giovanni Pace <g.pace@braintechinnovations.it>
2026-02-05 10:18:34 +0100

    Aggiornamento MTM

--- lez06_mtm_scuola.sql
+++ lez06_mtm_scuola.sql
@@ -14,3 +14,30 @@ CREATE TABLE Esame(
     data_esame DATE NOT NULL
 );
 
+CREATE TABLE Studente_Esame(
+	studenteRIF INTEGER NOT NULL,
+    esameRIF INTEGER NOT NULL,
+    FOREIGN KEY(studenteRIF) REFERENCES Studente(studenteID) ON DELETE CASCADE,
+    FOREIGN KEY(esameRIF) REFERENCES Esame(esameID) ON DELETE CASCADE,
+    PRIMARY KEY(studenteRIF, esameRIF)
+);
+
+INSERT INTO Studente(nome, cognome, matricola) VALUES
+("Giovanni", "Pace", "AB12345"),
+("Valeria", "Verdi", "AB12346"),
+("Marika", "Mariko", "AB12347");
+
+INSERT INTO Esame(nome, data_esame) VALUES
+("Programmazione", "2026-01-01"),
+("Programmazione", "2026-01-02"),
+("Basi di dati", "2026-01-03"),
+("Fisica", "2026-01-04"),
+("Filosofia", "2026-01-05");
+
+INSERT INTO Studente_Esame(studenteRIF, esameRIF) VALUES
+(1,1),
+(1,3),
+(2,1),
+(2,4);
+
+
