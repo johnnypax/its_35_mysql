d33eb338a289a37f78ea6298f57ed2abe81e7f45
Giovanni Pace <g.pace@braintechinnovations.it>
2026-03-26 09:14:25 +0100

    Aggiunti doc

---
+++ NoSQL/documenti.js
@@ -0,0 +1,73 @@
+[
+  {
+    "nome": "Luca",
+    "cognome": "Rossi",
+    "eta": 21,
+    "email": "luca.rossi@email.com",
+    "corso": "Informatica",
+    "mediaVoti": 27.5
+  },
+  {
+    "nome": "Giulia",
+    "cognome": "Bianchi",
+    "eta": 23,
+    "email": null,
+    "corso": "Matematica"
+  },
+  {
+    "nome": "Marco",
+    "cognome": "Verdi",
+    "eta": 20,
+    "corso": "Fisica",
+    "mediaVoti": 25.0
+  },
+  {
+    "nome": "Sara",
+    "cognome": "Neri",
+    "email": "sara.neri@email.com",
+    "corso": "Chimica",
+    "mediaVoti": 28.2
+  },
+  {
+    "nome": "Alessandro",
+    "cognome": "Conti",
+    "eta": 24,
+    "email": "ale.conti@email.com"
+  },
+  {
+    "nome": "Francesca",
+    "cognome": "Gallo",
+    "eta": 22,
+    "email": "francesca.gallo@email.com",
+    "corso": "Biologia",
+    "mediaVoti": null
+  },
+  {
+    "nome": "Davide",
+    "cognome": "Ferrari",
+    "eta": 25,
+    "corso": "Ingegneria",
+    "mediaVoti": 26.1
+  },
+  {
+    "nome": "Elena",
+    "cognome": "Romano",
+    "email": "elena.romano@email.com",
+    "corso": "Lettere"
+  },
+  {
+    "nome": "Simone",
+    "cognome": "Greco",
+    "eta": 23,
+    "email": null,
+    "mediaVoti": 24.8
+  },
+  {
+    "nome": "Chiara",
+    "cognome": "Lombardi",
+    "eta": 21,
+    "email": "chiara.lombardi@email.com",
+    "corso": "Economia",
+    "mediaVoti": 29.0
+  }
+]
